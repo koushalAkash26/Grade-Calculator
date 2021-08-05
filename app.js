f40195f78ed1a7720d7887d7c3d09723f9827170
koushal akash <koushalakash@koushals-MacBook-Air.local>
2021-08-05 23:55:31 +0530

    done

--- app.js
+++ app.js
@@ -78,12 +78,14 @@ function getResult(credit, grade, totalCredit) {
     return result;
   }
 function renderSub(sem) {
+
     let html = `
     <div class="row">
       <div class="col-8 head">Subject</div>
       <div class="col-2 head">Credits</div>
       <div class="col-2 head">Grade</div>
     </div>`;
+   
     for (let i = 1; i <= sem; i++) {
       html += `<div class="row">
     <div class="col-8"><input class="form-control " type="text" value="" inputmode="text"  /></div>
@@ -97,8 +99,8 @@ function renderSub(sem) {
                 <option value="0">0</option>
                 
               </select>
-    </div>
-    <div class="col-2">
+    </div>`
+    html+=`<div class="col-2">
       <select class="form-control sub-sgpag " id="grade">
         <option value = 10 >O</option>
         <option value = 9 >A+</option>
@@ -113,16 +115,60 @@ function renderSub(sem) {
   </div>`;
     }
     html +=
-      '<div id="alert-own"></div><button type="button" id="second" class="btn btn-secondary btn-sm btn-block all-button mb-4" >Calculate SGPA</button><div><div id="result"></div>';
+      '<div id="alert-own"></div><button type="button" id="second" class="btn btn-secondary btn-sm btn-block all-button mb-4" >Calculate SGPA</button><div><div id="result-1"></div>';
     html += `</div>`;
     window.scrollTo(0, document.body.scrollHeight/5 );
     document.getElementById('box1').innerHTML = html;
   }
+  function renderSub2(data){
+    let html = `
+    <div class="row">
+      <div class="col-8 head">Subject</div>
+      <div class="col-2 head">Credits</div>
+      <div class="col-2 head">Grade</div>
+    </div>`;
+    for(let i=0;i<data.subject.length;i++){
+        
+        html+= `<div class="row">
+      <div class="col-8"><input type="text" class="form-control" value="${data.subject[i].subject}" disabled/></div>
+      <div class="col-2 ">
+        
+        <select class="form-control sub-sgpac" value="${data.subject[i].credit}" disabled>
+                  <option value="${data.subject[i].credit}">${data.subject[i].credit}</option>
+                  
+                  
+                </select>
+      </div>`
+      html+=`<div class="col-2">
+        <select class="form-control sub-sgpag " id="grade">
+          <option value = 10 >O</option>
+          <option value = 9 >A+</option>
+          <option value = 8 >A</option> 
+          <option value = 7 >B+</option>
+          <option value = 6 >B</option>
+          <option value = 0 >RA</option>
+          <option value = 0>AB</option>
+          <option value = 0>P</option>
+        </select>
+      </div>
+    </div>`;
+      }
+      html +=
+        '<div id="alert-own"></div><button type="button" id="sixButton" class="btn btn-secondary btn-sm btn-block all-button mb-4" >Calculate SGPA</button><div><div id="result-2"></div>';
+      html += `</div>`;
+      window.scrollTo(0, document.body.scrollHeight/5 );
+      document.getElementById('box1').innerHTML = html;
+
+    }
+
+  
+  
   function getSGPA(totalSemesters) {
     let SGPAC = document.querySelectorAll('.sub-sgpac');
     console.log(SGPAC)
     let SGPAG = document.querySelectorAll('.sub-sgpag');
     console.log(SGPAG)
+   
 
     const arSGPAC=[]
     const arSGPAG=[]
@@ -144,29 +190,108 @@ function renderSub(sem) {
      console.log(typeof(sem))
      localStorage.setItem(sem,resSgpa)
      localStore()
-     const result = document.getElementById('result');
+     const result = document.getElementById('result-1');
       result.innerHTML = `<div class="alert alert-primary" role="alert" id="result">
       Semester Grade Point Average (SGPA) is
       <span class="alert-link">${resSgpa}</span>
     </div>`;
     window.scrollTo(0, document.body.scrollHeight /5);
 }
+function getSGPA2(data){
+  totalSemesters=data.subject.length
+  let SGPAC = document.querySelectorAll('.sub-sgpac');
+    let SGPAG = document.querySelectorAll('.sub-sgpag');
+    
+
+    const arSGPAC=[]
+    const arSGPAG=[]
+    for (let i = 0; i < totalSemesters; i++) {
+        let cred = SGPAC[i].value;
+        let grad = SGPAG[i].value;
+        
+        arSGPAC.push(Number(cred));
+        arSGPAG.push(Number(grad));
+
+
+    }
+    console.log(`credits=>${arSGPAC}`)
+    console.log(`Grade=>${arSGPAG}`)
+     let tc=arSGPAC.reduce((a, b) => a + b, 0)
+     let resSgpa=getResult(arSGPAC,arSGPAG,tc)
+     let sem=document.getElementById("semester").value
+     console.log(sem)
+     console.log(typeof(sem))
+     localStorage.setItem(sem,resSgpa)
+     localStore()
+     const result = document.getElementById('result-2');
+      result.innerHTML = `<div class="alert alert-primary" role="alert" id="result">
+      Semester Grade Point Average (SGPA) is
+      <span class="alert-link">${resSgpa}</span>
+    </div>`;
+    window.scrollTo(0, document.body.scrollHeight /5);
+
+}
+
   var fb=document.getElementById("firstButtton");
   console.log(fb)
   fb.addEventListener('click',()=>{
-     let NoofSub=document.getElementById("noofSub").value;
-     console.log(NoofSub)
-      console.log("hello")
+     if(document.getElementById("dept").value==="other"){
+       console.log("nadakatum nadakattum")
+       let html=`<div class="row">
+       <div class="col-8"><label for="noofSub">Number of subjects</label></div>
+       <div class="col-4" >
+         <select class="form-control "  id="noofSub">
+           <option value="1">1</option>
+           <option value="2">2</option>
+           <option value="3">3</option>
+           <option value="4">4</option>
+           <option value="5">5</option>
+           <option value="6">6</option>
+           <option value="7">7</option>
+           <option value="8">8</option>
+         </select>
+       </div>
+     </div>`
+     html +=
+     '<div id="alert-own"></div><button type="button" id="fifthButton" class="btn btn-secondary btn-sm btn-block all-button mb-4" >Calculate </button><div><div id="result"></div>';
+   html += `</div>`;
+      let cct=document.getElementById("box3")
+      cct.innerHTML=html
+      let fob=document.getElementById("fifthButton")
+      fob.addEventListener('click',()=>{
+      let NoofSub=document.getElementById("noofSub").value;
       renderSub(NoofSub);
       var sb=document.getElementById("second");
-        console.log(sb)
-        sb.addEventListener('click',()=>{
-    
-        getSGPA(NoofSub)
-        console.log("HEYY")
-     
+      console.log(sb)
+      sb.addEventListener('click',()=>{
 
-  })
+      getSGPA(NoofSub)
+      console.log("HEYY")
+
+
+      })
+
+      })
+
+      
+
+      
+
+     } 
+     else{
+      let deptTrial=document.getElementById('dept').value
+      let regulation=document.getElementById('regulation').value
+      let semester=document.getElementById('semester').value
+      const data=json[regulation][deptTrial][semester]
+      renderSub2(data)
+      let sib=document.getElementById("sixButton")
+      sib.addEventListener('click',()=>{
+        getSGPA2(data)
+
+      })
+
+     }
+     
      
 
   })
@@ -202,6 +327,7 @@ function renderSub(sem) {
   })
 
 })
+
 for(let i=0;i<8;i++){
   localStorage.removeItem(i);
 }
